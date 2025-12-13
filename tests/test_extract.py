4c0889dacaff87daa16a3a892084178db9ddc40a
Christophe-dot <christophe.pellerin@laposte.fr>
2025-12-13 22:33:50 +0100

    28_commit

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import os
 import requests
 from dotenv import find_dotenv, load_dotenv
-from app.extract import extract, get_api
+from app_backup.extract import extract, get_api
 import logging
 
 # Charger le .env
